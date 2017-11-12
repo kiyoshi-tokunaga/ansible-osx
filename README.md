2347ae8afad5e9bdb4eb01c2fc090d98bf713483
kiyoshi-tokunaga <ponkiy.recruit@gmail.com>
2017-11-12 22:20:23 +0900

    Add setup ansible

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ansible-osx
 Mac OS X用のセットアップ
+
+## 手順
+1. System Update & Restart
+`$ sudo softwareupdate --install --recommended`
+`$ sudo shutdown -r now`
+2. Setup Ansible
+`$ make setup`
---
+++ install_ansible.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+which pip >/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+    echo "> not found command pip"
+    echo "> install pip"
+    # easy_install pip
+fi
+
+which ansible >/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+    echo "> not found command ansible"
+    echo "> install ansible"
+    # pip install ansible
+fi
