74642a2bbc9267927f5e4afa4f09c9a321838b15
kiyoshi-tokunaga <ponkiy.recruit@gmail.com>
2017-11-12 22:32:11 +0900

    Fix install ansible script

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-which pip >/dev/null 2>&1
-if [ $? -ne 0 ];
-then
+which pip 2>&1
+if [ $? -ne 0 ]; then
     echo "> not found command pip"
     echo "> install pip"
-    sudo easy_install pip
+    easy_install pip
 fi
 
-which ansible >/dev/null 2>&1
-if [ $? -ne 0 ];
-then
+which ansible 2>&1
+if [ $? -ne 0 ]; then
     echo "> not found command ansible"
     echo "> install ansible"
-    sudo pip install ansible
+    pip install ansible
 fi
